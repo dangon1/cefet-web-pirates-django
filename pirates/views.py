524579511edebae77bb0714b361d0a82c3d3a542
Daniel Gonçalves <d.aguiargoncalves@gmail.com>
2018-11-25 17:34:12 -0200

    inserindo e deletando

--- pirates/views.py
+++ pirates/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import View
+from django.db.models import F, ExpressionWrapper, DecimalField, Sum
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from pirates.models import Tesouro
 
-class ListaTesourosView(TemplateView):
-    template_name = "lista_tesouros.html"
+class TesouroForm(forms.ModelForm):
+    class Meta:
+        model = Tesouro
+        fields = ['nome', 'quantidade', 'preco', 'img_tesouro']
+        labels = {
+            'nome': 'Nome',
+            'quantidade': 'Quantidade',
+            'preco': 'Preço',
+            'img_tesouro': 'Imagem'
+		}
 
-    def get(self, request):
-    	return render(request,
-              "lista_tesouros.html",
-              {'sobre': [{"titulo":"De onde eles vêm",
-                          "texto":"Lorem ipsum dolor sit amet, consectetur"},
-                          {"titulo":"O que eles querem",
-                          "texto":"osakdpokasdokaspok"},
-                          ]})
+class ListaTesourosView(View):
+  def get(self, request):
+    return render(request, 'lista_tesouros.html',
+        {'lista_tesouros': Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('preco')*F('quantidade'),output_field=DecimalField(max_digits=10, decimal_places=2, blank=True))),
+         'total_geral': Tesouro.objects.aggregate(total_geral=Sum(ExpressionWrapper(F('preco')*F('quantidade'),output_field=DecimalField(max_digits=10, decimal_places=2, blank=True))))['total_geral']
+        })
 
-#    	return render(request,
-#              {'lista_tesouros': <Fazer uma busca pelos tesouros>,
- # 				'total_geral': <Somatório do valor de todos os tesouros>
-#			  })
-#from django.db.models import Count
-#Autor.objects.annotate(Count('post'))
+class SalvarTesouro(View):
+    def get(self, request, id=None):
+        tesouro = Tesouro.objects.get(pk=id) if id != None else None
+        return render(request, 'salvar_tesouro.html', {'tesouro': TesouroForm(instance=tesouro)})
+    
+    def post(self, request, id=None):
+        tesouro = Tesouro.objects.get(pk=id) if id != None else None
+        form = TesouroForm(request.POST, request.FILES, instance=tesouro)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('lista_tesouros'))
+        else:
+            return render(request, 'salvar_tesouro.html', {'tesouro': form})
+
+class RemoverTesouro(View):
+	def get(self, request, id):
+		Tesouro.objects.get(pk=id).delete()
+		return HttpResponseRedirect(reverse('lista_tesouros'))
